719f6ffe719c59b91041ed1a8efc448733e31c87
Daniel <danielnizabekov@gmail.com>
2021-02-10 16:49:20 +0600

    V2

--- Assets/Scripts/Brush.cs
+++ Assets/Scripts/Brush.cs
@@ -7,15 +7,24 @@ public class Brush : MonoBehaviour
     [SerializeField]
     GameObject paintCube;
 
+    [SerializeField]
+    Material brushMaterial;
+    [SerializeField]
+    Material paintMaterial;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material = brushMaterial;
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            paintCube.GetComponentInChildren<Renderer>().material = paintMaterial;
             GameObject tmp = Instantiate(paintCube, transform.parent.position, transform.parent.rotation);
             float rotationCurrent = transform.parent.eulerAngles.y;
             float normalizeRotation = Mathf.FloorToInt((rotationCurrent / 30))*30;
-            Debug.Log(normalizeRotation);
-            
             tmp.transform.rotation = Quaternion.Euler(tmp.transform.rotation.x, Mathf.Abs(normalizeRotation), tmp.transform.rotation.z);
         }
     }
